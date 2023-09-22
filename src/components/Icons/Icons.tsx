a7c24b65ab7f18db8178ac0d46e6817ffb095ed7
cwighty <agentcboy@gmail.com>
2023-09-21 22:45:21 -0600

    wip toasts

---
+++ src/components/Icons/Icons.tsx
@@ -0,0 +1,17 @@
+const CloseIcon = () => (
+    <i className="bi bi-x-lg" aria-hidden="true"></i>
+  );
+  
+  const SuccessIcon = () => (
+    <i className="bi bi-check-lg" aria-hidden="true"></i>
+  );
+  
+  const FailureIcon = () => (
+    <i className="bi bi-x-lg" aria-hidden="true"></i>
+  );
+  
+  const WarningIcon = () => (
+    <i className="bi bi-exclamation-lg" aria-hidden="true"></i>
+  );
+  
+  export { CloseIcon, SuccessIcon, FailureIcon, WarningIcon };
